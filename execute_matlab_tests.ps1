e4205602e4d7099095b0e2d077dcaaf7ae4f52de
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-02-02 11:02:07 +0000

    Add utility import to pwsh script

--- execute_matlab_tests.ps1
+++ execute_matlab_tests.ps1
@@ -1,3 +1,7 @@
+. $PSScriptRoot/powershell_helpers.ps1 <# Imports:
+  Write-And-Invoke
+#>
+
 $ErrorActionPreference = 'Stop'
 
 $MATLAB_VERSION_MAP = @{
@@ -14,7 +18,7 @@ Write-And-Invoke 'conda activate py36_pace_integration'
 Set-Item -Path Env:PYTHON_EX_PATH -Value (Get-Command python).Source
 
 # Get Matlab root directory from registry
-$MATLAB_ROOT = (Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION])).MATLABROOT
+$MATLAB_ROOT = (Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\MathWorks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION])).MATLABROOT
 
 # Set up Matlab and run tests
 $MATLAB_PROCESS = Start-Process -FilePath "$MATLAB_ROOT\bin\matlab.exe" -Wait -PassThru -ArgumentList '-nosplash', '-nodesktop', '-wait', '-batch', 'setup_and_run_tests'
