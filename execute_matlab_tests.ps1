0ba82814aa6582c457be740d2c86a63528e8f08e
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-02-02 13:42:56 +0000

    Catch registry key error

--- execute_matlab_tests.ps1
+++ execute_matlab_tests.ps1
@@ -18,9 +18,22 @@ Write-And-Invoke 'conda activate py36_pace_integration'
 Set-Item -Path Env:PYTHON_EX_PATH -Value (Get-Command python).Source
 
 # Get Matlab root directory from registry
-$MATLAB_ROOT = (Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SOFTWARE\MathWorks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION])).MATLABROOT
+Try {
+  $MATLAB_REG = Get-ItemProperty `
+    Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION]) `
+    -ErrorAction Stop
+} Catch [System.Management.Automation.ItemNotFoundException] {
+  Write-Error ("Couldn't find Matlab in the Windows registry, ensure this Matlab version is " +
+               "definitely installed and the correct Powershell architecture is being used. A 32-bit " +
+	       "Powershell may not be able to search a 64-bit registry and vice versa`n$($_.Exception)")
+}
+$MATLAB_ROOT = ($MATLAB_REG).MATLABROOT
 
 # Set up Matlab and run tests
-$MATLAB_PROCESS = Start-Process -FilePath "$MATLAB_ROOT\bin\matlab.exe" -Wait -PassThru -ArgumentList '-nosplash', '-nodesktop', '-wait', '-batch', 'setup_and_run_tests'
+$MATLAB_PROCESS = Start-Process `
+  -FilePath "$MATLAB_ROOT\bin\matlab.exe" `
+  -Wait `
+  -PassThru `
+  -ArgumentList '-nosplash', '-nodesktop', '-wait', '-batch', 'setup_and_run_tests'
 
 exit $MATLAB_PROCESS.ExitCode
