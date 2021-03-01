7def475be1b933a8834592bb97bc2b073099ada1
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-03-01 12:36:05 +0000

    Run tests from powershell without system.diagnostics.Process
    Process was causing strange errors where the build would hang, or
    the full output wasn't passed to the console.

--- powershell_scripts/execute_matlab_tests.ps1
+++ powershell_scripts/execute_matlab_tests.ps1
@@ -24,26 +24,4 @@ Write-And-Invoke "Set-Item -Path Env:PYTHON_EX_PATH -Value $PYTHON_EX_PATH"
 $MATLAB_REG = Get-From-Registry "HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION])"
 $MATLAB_ROOT = ($MATLAB_REG).MATLABROOT
 
-# Set up Matlab and run tests
-# Must be run via System.Diagnostics rather than Start-Process to redirect
-# output to stdout/err and enable it to be seen in the powershell console
-$pinfo = New-Object System.Diagnostics.ProcessStartInfo
-$pinfo.FileName = "$MATLAB_ROOT\bin\matlab.exe"
-$pinfo.RedirectStandardError = $true
-$pinfo.RedirectStandardOutput = $true
-$pinfo.UseShellExecute = $false
-$pinfo.Arguments = '-nosplash', '-nodesktop', '-wait', '-batch', 'setup_and_run_tests'
-$pinfo.WorkingDirectory = $Env:WORKSPACE
-$pinfo | Select-Object
-
-$p = New-Object System.Diagnostics.Process
-$p.StartInfo = $pinfo
-$p.Start() | Out-Null
-$p.WaitForExit()
-$stdout = $p.StandardOutput.ReadToEnd()
-$stderr = $p.StandardError.ReadToEnd()
-Write-Output "stdout: $stdout"
-Write-Output "stderr: $stderr"
-Write-Output "exit code: " + $p.ExitCode
-
-exit $p.ExitCode
+. $MATLAB_ROOT\bin\matlab.exe -nosplash -nodesktop -wait -batch "setup_and_run_tests"
