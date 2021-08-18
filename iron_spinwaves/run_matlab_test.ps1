4d304883a59eeb44708775a60395aab43817f1de
Duc Le <duc.le@stfc.ac.uk>
2021-08-18 14:39:28 +0100

    Add SpinW integration test (#4)
    * Add SpinW integration test
    
    * SpinW integration - cleanup

---
+++ iron_spinwaves/run_matlab_test.ps1
@@ -0,0 +1,30 @@
+. $PSScriptRoot/../powershell_scripts/powershell_helpers.ps1 <# Imports:
+  Get-From-Registry
+#>
+
+<#
+  .SYNOPSIS
+    Runs a Matlab script to run integration tests
+  .DESCRIPTION
+    Gets the Matlab command from registry and runs it
+  .NOTES
+    Required environment variables:
+      MATLAB_VERSION - Matlab version to use e.g. 2019b
+#>
+
+$ErrorActionPreference = 'Stop'
+
+$MATLAB_VERSION_MAP = @{
+  '2018a' = '9.4';
+  '2018b' = '9.5';
+  '2019a' = '9.6';
+  '2019b' = '9.7';
+  '2020a' = '9.8';
+  '2020b' = '9.9';
+}
+
+# Get Matlab root directory from registry, and path to MATLAB exe
+$MATLAB_REG = Get-From-Registry "HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB\$($MATLAB_VERSION_MAP[$Env:MATLAB_VERSION])"
+$MATLAB_ROOT = ($MATLAB_REG).MATLABROOT
+
+. $MATLAB_ROOT/bin/matlab.exe -nosplash -nodesktop -batch "run('iron_spinwaves/run_spinw_horace_test.m')"
---
+++ iron_spinwaves/run_spinw_horace_test.m
@@ -0,0 +1,63 @@
+this_dir = fileparts(mfilename('fullpath'));
+parent_dir = split(this_dir, filesep);
+parent_dir = join(parent_dir(1:end-1), filesep);
+parent_dir = parent_dir{1};
+cd(fullfile(parent_dir, 'Horace'));
+horace_install;
+horace_on;
+addpath(genpath(fullfile(parent_dir, 'spinw')));
+cd(parent_dir);
+
+% Reads in the cut data
+w_fe = read_sqw(fullfile(this_dir, 'fe_cut.sqw'));
+
+% Set up SpinW model
+a = 2.87;
+fe = spinw();
+fe.genlattice('lat_const', [a, a, a], 'angled', [90, 90, 90], 'spgr', 'I m -3 m')
+fe.addatom('label', 'MFe3', 'r', [0, 0, 0], 'S', 5/2, 'color', 'gold')
+fe.gencoupling()
+fe.addmatrix('label', 'J1', 'value', 1, 'color', 'gray')
+fe.addmatrix('label', 'D', 'value', diag([0, 0, -1]), 'color', 'green')
+fe.addcoupling('mat', 'J1', 'bond', 1)
+fe.addaniso('D')
+fe.genmagstr('mode', 'direct', 'S', [0., 0., 1.; 0., 0., 1.]');
+
+% Set up multi-fit
+% Starting parameters for fit
+J = 35;     % Exchange interaction in meV
+D = 0;      % Single-ion anisotropy in meV
+gam = 30;   % Intrinsic linewidth in meV (inversely proportional to excitation lifetime)
+temp = 10;  % Sample measurement temperature in Kelvin
+amp = 300;  % Magnitude of the intensity of the excitation (arbitrary units)
+
+cpars = {'mat', {'J1', 'D(3,3)'}, 'hermit', false, 'optmem', 1, ...
+         'useFast', true, 'resfun', 'sho', 'formfact', true};
+
+kk = multifit_sqw(w_fe);
+kk = kk.set_fun (@fe.horace_sqw, {[J, D, gam, temp, amp], cpars{:}});
+kk = kk.set_free ([1, 0, 1, 0, 1]);
+kk = kk.set_bfun (@linear_bg, [0.1, 0]);
+kk = kk.set_bfree ([1, 0]);
+kk = kk.set_options ('list', 2);
+
+tic();
+wsim = kk.simulate();
+t_spinw_single = toc();
+fprintf('Time to evaluate a single iteration: %s s\n', t_spinw_single);
+
+tic();
+[wfit, fitpars] = kk.fit();
+t_spinw_fit = toc();
+fprintf('Time to run fit: %s s\n', t_spinw_fit);
+
+if sum(abs(fitpars.p - [15.2440 0 65.9076 10 135.4074])) > 0.1
+  disp('FAILURE: fitted parameters found as');
+  disp(fitpars.p);
+  disp('Expected:');
+  disp([15.2440 0 65.9076 10 135.4074]);
+  quit(1);
+else
+  disp('SUCCESS');
+end
+pause(1);
